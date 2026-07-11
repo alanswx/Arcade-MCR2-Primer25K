0e7400d8d5dc9b72044aac4b0f6060cd4ee524fe
Alan Steremberg <alan.steremberg@gmail.com>
2026-07-11 16:00:31 -0400

    Update Tang Primer 25K build paths and verify top PMOD DVI pin mapping

--- mcr2_primer25k/build.tcl
+++ mcr2_primer25k/build.tcl
@@ -18,31 +18,31 @@ set_option -multi_boot 1
 
 # Add source files
 add_file src/mcr2_primer25k_top.sv
-add_file src/dpram.sv
-add_file src/gen_ram.sv
-add_file src/gowin_pll/gowin_pll_mcr2.v
-add_file src/dvi_tx/tmds_encoder.sv
-add_file src/dvi_tx/hdmi_tx.sv
-add_file src/audio/ds_dac.sv
+add_file ../src/rtl/dpram.sv
+add_file ../src/rtl/gen_ram.sv
+add_file ../src/rtl/gowin_pll_mcr2.v
+add_file ../src/dvi_tx/tmds_encoder.sv
+add_file ../src/dvi_tx/hdmi_tx.sv
+add_file ../src/audio/ds_dac.sv
 
 # Add core files (VHDL/SystemVerilog)
-add_file src/rtl/mcr2.vhd
-add_file src/rtl/mcr_sound_board.vhd
-add_file src/rtl/YM2149_linmix_sep.vhd
-add_file src/rtl/midssio_82s123.vhd
-add_file src/rtl/cmos_ram.vhd
-add_file src/rtl/spinner.sv
+add_file ../src/rtl/mcr2.vhd
+add_file ../src/rtl/mcr_sound_board.vhd
+add_file ../src/rtl/YM2149_linmix_sep.vhd
+add_file ../src/rtl/midssio_82s123.vhd
+add_file ../src/rtl/cmos_ram.vhd
+add_file ../src/rtl/spinner.sv
 
 # Add CPU/CTC VHDL files
-add_file src/rtl/T80/t80_1_alu.vhd
-add_file src/rtl/T80/t80_2_mcode.vhd
-add_file src/rtl/T80/t80_3_reg.vhd
-add_file src/rtl/T80/t80_4_core.vhd
-add_file src/rtl/T80/t80_5_pa.vhd
-add_file src/rtl/T80/t80_6_s.vhd
-add_file src/rtl/Z80CTC/ctc_controler.vhd
-add_file src/rtl/Z80CTC/ctc_counter.vhd
-add_file src/rtl/Z80CTC/z80ctc_top.vhd
+add_file ../src/rtl/T80/t80_1_alu.vhd
+add_file ../src/rtl/T80/t80_2_mcode.vhd
+add_file ../src/rtl/T80/t80_3_reg.vhd
+add_file ../src/rtl/T80/t80_4_core.vhd
+add_file ../src/rtl/T80/t80_5_pa.vhd
+add_file ../src/rtl/T80/t80_6_s.vhd
+add_file ../src/rtl/Z80CTC/ctc_controler.vhd
+add_file ../src/rtl/Z80CTC/ctc_counter.vhd
+add_file ../src/rtl/Z80CTC/z80ctc_top.vhd
 
 # Add constraints files
 add_file -type cst src/mcr2_primer25k.cst
